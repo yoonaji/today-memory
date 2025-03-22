b9bd52c5586e00b0d8b1e79bb0c92ea15ce3366d
yoonaji <yunaji0824@gmail.com>
2025-03-22 11:55:23 +0900

    first commit

---
+++ src/main/java/com/springboot/today_memory/TodayMemoryApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.today_memory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayMemoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodayMemoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/today_memory/TodayMemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.today_memory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayMemoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
